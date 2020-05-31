75841d703b7d15a8705db324521408ef7e1effbb
Rohit-Mundada <rohitrm0801@gmail.com>
2020-05-31 11:48:19 +0530

    Added login form

--- src/index.js
+++ src/index.js
@@ -22,6 +22,8 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 
 // core components
 import Admin from "layouts/Admin.js";
+// Login component
+import SignInSide from "./layouts/Login";
 
 import "assets/css/material-dashboard-react.css?v=1.9.0";
 
@@ -30,6 +32,7 @@ const hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
+      <Route path="/login" component={SignInSide} />
       <Route path="/admin" component={Admin} />
       <Redirect from="/" to="/admin/dashboard" />
     </Switch>
