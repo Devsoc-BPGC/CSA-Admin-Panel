75ab0fdde0e1f027a5fdde33b6e99efbf950fa66
Rohit-Mundada <rohitrm0801@gmail.com>
2020-06-01 11:05:30 +0530

    Added Google Sign in

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 // core components
 import Admin from "layouts/Admin.js";
 // Login component
-import SignInSide from "./layouts/Login";
+import Login from "./layouts/Login";
 
 import "assets/css/material-dashboard-react.css?v=1.9.0";
 
@@ -32,9 +32,9 @@ const hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Route path="/login" component={SignInSide} />
+      <Route path="/login" component={Login} />
       <Route path="/admin" component={Admin} />
-      <Redirect from="/" to="/admin/dashboard" />
+      <Redirect from="/" to="/login" />
     </Switch>
   </Router>,
   document.getElementById("root")
--- src/layouts/Login.js
+++ src/layouts/Login.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 // import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
@@ -14,16 +13,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import Campus from "../assets/img/campus.jpg";
 import axios from "axios";
 import GoogleLogin from "react-google-login";
-// // Firebase authentication
-// import * as firebase from "firebase/app";
-// // Import firebase config
-// import firebaseConfig from "../firebase-config";
-// // Importing auth and firestore
-// import "firebase/auth";
-// import "firebase/firestore";
-
-// // Initialising Firebase
-// firebase.initializeApp(firebaseConfig);
+// Admin page
+import Admin from "./Admin";
 
 function Copyright() {
   return (
@@ -52,8 +43,6 @@ const useStyles = makeStyles((theme) => ({
         : theme.palette.grey[900],
     backgroundSize: "cover",
     backgroundPosition: "center",
-    width: "100%",
-    height: "100vh",
   },
   paper: {
     margin: theme.spacing(8, 4),
@@ -65,9 +54,15 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
+  welcome: {
+    width: "100%",
+    marginTop: theme.spacing(2),
+    textAlign: "center",
+    marginBottom: theme.spacing(2),
+  },
   form: {
     width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+    marginTop: theme.spacing(2),
   },
   submit: {
     width: "100%",
@@ -75,95 +70,87 @@ const useStyles = makeStyles((theme) => ({
   },
   copyright: {
     bottom: "0",
-    marginTop: "55vh",
+    marginTop: "30vh",
   },
 }));
 
-export default function SignInSide() {
+export default function Login() {
   const classes = useStyles();
-  const [email, setEmail] = React.useState("example@mail.com");
+  const [email, setEmail] = React.useState("");
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-  // const [loading, isLoading] = React.useState(false);
 
   const responseGoogle = (response) => {
     console.log(response);
+    setEmail(response.profileObj.email);
+    axios({
+      method: "get",
+      url: "https://jsonplaceholder.typicode.com/",
+      data: JSON.stringify({
+        email: email,
+      }),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+      .then((res) => {
+        console.log(res);
+
+        setIsLoggedIn(true);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoggedIn(true);
+      });
   };
 
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image}>
-        <img src={Campus} alt="campus" />
-      </Grid>
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Login
-          </Typography>
-          <form className={classes.form}>
-            {/* <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            /> */}
-            {/* <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            /> */}
-            {/* <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
+      <>
+        {isLoggedIn ? (
+          <Admin />
+        ) : (
+          <>
+            <Grid item xs={false} sm={4} md={7} className={classes.image}>
+              <img src={Campus} alt="campus" />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sm={8}
+              md={5}
+              component={Paper}
+              elevation={6}
+              square
             >
-              Login with Google
-            </Button> */}
-            <GoogleLogin
-              clientId="356883126789-kr191fl5f5odmmb8c9lr0dspapq41rlb.apps.googleusercontent.com"
-              buttonText="Login with Google"
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              className={classes.submit}
-            />
-            {/* <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid> */}
-            <Box className={classes.copyright}>
-              <Copyright />
-            </Box>
-          </form>
-        </div>
-      </Grid>
+              <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography
+                  component="h3"
+                  variant="h3"
+                  className={classes.welcome}
+                >
+                  Welcome to the <p>CSA Admin Panel</p>
+                </Typography>
+                <form className={classes.form}>
+                  <GoogleLogin
+                    clientId="604242605681-v60kba2hglvtnspec2mtbacta51r64mp.apps.googleusercontent.com"
+                    buttonText="Login with Google"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    className={classes.submit}
+                  />
+                  <Box className={classes.copyright}>
+                    <Copyright />
+                  </Box>
+                </form>
+              </div>
+            </Grid>
+          </>
+        )}
+      </>
     </Grid>
   );
 }
